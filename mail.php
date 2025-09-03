49d3abd2393b2636baae55b35a7fbfad3a2794b2
yehia <yehiakobeyssy2018@gmail.com>
2025-09-03 16:29:49 +0200

    Fix the Mail php

--- mail.php
+++ mail.php
@@ -1,7 +1,7 @@
 <?php
 // Import PHPMailer classes into the global namespace
 // These must be at the top of your script, not inside a function
-$applicationemail='yktechnologyapp@kawnex.com';
+$applicationemail='kawnexapp@kawnex.com';
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
@@ -13,18 +13,14 @@ require 'Mailer/autoload.php';
 $mail = new PHPMailer();
 
     //Server settings
-    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                    // Enable verbose debug output
-    $mail->isSMTP();                                            // Send using SMTP
-    $mail->Host       = 'smtp.titan.email';                       // Set the SMTP server to send through
-    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-    $mail->Username   = 'yktechnologyapp@kawnex.com';            // SMTP username
-    $mail->Password   = 'Mounir@141168';       //Mounir@141168            // SMTP password
-    $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            // Enable implicit TLS encryption
-    $mail->Port       = 587;                                    // SMTP password
-
-    $mail->SMTPSecure = 'tls';                             // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-    $mail->Port       = 587;                                    // TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
+    //$mail->SMTPDebug = SMTP::DEBUG_SERVER;           // لتفاصيل الأخطاء (يمكن تغييره لـ 0 بعد التأكد)
+    $mail->isSMTP();
+    $mail->Host       = 'smtp.hostinger.com';       // خادم SMTP الخاص بـ Hostinger
+    $mail->SMTPAuth   = true;                        // تفعيل المصادقة
+    $mail->Username   = 'kawnexapp@kawnex.com';     // بريدك كامل
+    $mail->Password   = 'Maher@141168';        // استبدلها بـ App Password إذا كان الحساب محمي
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS; // TLS على المنفذ 587
+    $mail->Port       = 587;   
 // Content
 $mail->isHTML(true);  
 $mail->CharSet = "UTF-8";
